2d36e325bed10158ca946b9150298e12f027d714
Tom Bartuzel <tom.bartuzel@epitech.eu>
2023-02-15 10:16:53 +0100

    my Spotify

--- youtubeToMp3/main.py
+++ youtubeToMp3/main.py
@@ -1,6 +1,18 @@
 import youtube_downloader
 import file_converter
 import sys
+import os
+import shutil
+
+# copy songs from one directory to another
+def copySongs(source="./songs/mp3/", destination="./../my_spotify/assets/song/"):
+    try:
+        os.mkdir(destination)
+    except FileExistsError:
+        pass
+    for filename in os.listdir(source):
+        shutil.copy(source + filename, destination)
+        os.remove(source + filename)
 
 if (len(sys.argv) == 2):
     link = sys.argv[1]
@@ -8,3 +20,4 @@ if (len(sys.argv) == 2):
     filename = youtube_downloader.download_video(link, 'low')
     print("Converting...")
     file_converter.convert_to_mp3(filename)
+    copySongs()
